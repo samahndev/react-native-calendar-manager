b7272a7c9013ac9b6523bcb4561dd2dbe78bc8db
Sam Ahn <sam.ahn.dev@gmail.com>
2020-04-03 14:12:51 +1300

    added date parameter

--- ios/CalendarManager.m
+++ ios/CalendarManager.m
@@ -5,9 +5,9 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(NSDate *)date)
 {
-    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location, date);
 }
 
 @end
