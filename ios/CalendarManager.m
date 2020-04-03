fb16388ba52f68d3aa4b75a0b23f18ae0fc138ce
Sam Ahn <sam.ahn.dev@gmail.com>
2020-04-03 13:06:41 +1300

    added missing ;

--- ios/CalendarManager.m
+++ ios/CalendarManager.m
@@ -7,7 +7,7 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
 {
-    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location)
+    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
 @end
